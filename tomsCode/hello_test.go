b794a7024d5677bf32997fc8d85ea5c848f087f3
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2023-04-22 09:15:34 -0400

    Complete "hello, world" for learn go with tdd

--- tomsCode/hello_test.go
+++ tomsCode/hello_test.go
@@ -4,13 +4,28 @@ import "testing"
 
 func TestHello(t *testing.T) { //I learned that test functions need to start with a capital T!!
 	t.Run("testing with a person's name", func(t *testing.T) {
-		got := hello("tom")
+		got := hello("tom", "")
 		want := "Hello, tom!"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("testing with no input", func(t *testing.T) {
-		got := hello("")
+		got := hello("", "")
 		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("spanish input", func(t *testing.T) {
+		got := hello("Elon", "Spanish")
+		want := "Hola, Elon!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("french input", func(t *testing.T) {
+		got := hello("Bill", "French")
+		want := "Bonjour, Bill!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("dutch input", func(t *testing.T) {
+		got := hello("Jack", "Dutch")
+		want := "Hallo, Jack!"
 		assertCorrectMessage(t, got, want)
 	})
 }
